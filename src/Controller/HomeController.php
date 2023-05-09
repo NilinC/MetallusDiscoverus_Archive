0de299302c7221e10656409431520c32f7a958c6
Aliette Ruppert <aliette.ruppert@gmail.com>
2023-05-09 15:55:24 +0200

    feat: Ajout des statistiques pour la liste d'écoute
    * Affichage du nombre d'albums ajoutés à la liste d'écoute
    * Affichage du nombre d'albums écoutés pendant le mois courant
    * Affichage du nombre d'albums écoutés depuis le début de l'année
    * Affichage du nombre d'albums sortis durant le mois courant

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,9 +20,23 @@ class HomeController extends AbstractController
     {
         $list = $this->albumRepository->findAllDescOrderedByReleaseDate();
 
+        $nbOfAlbumsToListen = $this->albumRepository->getNumberOfAlbumsAddedToListen();
+
+        $nbAlbumsListenedDuringCurrentMonth = $this->albumRepository->getNumberOfAlbumsListenedInCurrentMonth();
+
+        $nbAlbumsListenedSinceStartOfYear = $this->albumRepository->getNumberAlBumsListenedSinceStartOfYear();
+
+        $nbAlbumsReleasedDuringCurrentMonth = $this->albumRepository->getNbAlbumsReleasedDuringCurrentMonth();
+
         return $this->render(
             'base.html.twig',
-            [ 'list' => $list ],
+            [
+                'list' => $list,
+                'nb_albums_to_listen' => $nbOfAlbumsToListen,
+                'nb_albums_listened_during_current_month' => $nbAlbumsListenedDuringCurrentMonth,
+                'nb_albums_listened_since_start_of_year' => $nbAlbumsListenedSinceStartOfYear,
+                'nb_albums_released_during_current_month' => $nbAlbumsReleasedDuringCurrentMonth
+            ],
             new Response('OK', Response::HTTP_OK)
         );
     }
